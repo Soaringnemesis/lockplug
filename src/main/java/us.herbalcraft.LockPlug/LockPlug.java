439b5b15d18a9d219bb0023233e18a16adbffb11
Soaringnemesis <34289582+Soaringnemesis@users.noreply.github.com>
2020-05-04 21:40:08 -0700

    Update LockPlug.java

--- src/main/java/us.herbalcraft.LockPlug/LockPlug.java
+++ src/main/java/us.herbalcraft.LockPlug/LockPlug.java
@@ -1,10 +1,13 @@
 package us.herbalcraft.LockPlug;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LockPlug extends JavaPlugin {
+    FileConfiguration config = getConfig();
     @Override
     public void onEnable() {
-
+        this.saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new Events(), this);
     }
 }
